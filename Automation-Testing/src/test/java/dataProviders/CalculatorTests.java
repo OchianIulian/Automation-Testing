7c535578e31e8633de654d3dc43b17e1565fa15f
andrei <ochian_a@yahoo.ro>
2024-02-05 12:11:38 +0200

    feat: add first adding test

--- Automation-Testing/src/test/java/dataProviders/CalculatorTests.java
+++ Automation-Testing/src/test/java/dataProviders/CalculatorTests.java
@@ -1,4 +1,21 @@
 package dataProviders;
 
+import org.example.calculator.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class CalculatorTests {
+    static Calculator calculator;
+
+    @BeforeClass
+    public void setUp(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAddingPositiveNumbers(){
+        System.out.println("Verify that some:" + calculator.compute(4, 5, "+") + "is equal to 9" );
+        Assert.assertEquals(calculator.compute(4, 5, "+"), 9); 
+    }
 }
